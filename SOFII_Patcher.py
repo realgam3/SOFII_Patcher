c102c4c26b0db348f0b524bc8bb66de48be48b55
realgam3 <realgam3@gmail.com>
2020-02-05 22:07:57 +0200

    Added Registry Search For SoF2, Added Current Folder Search For SoF2, Added SoF2 Single Player Patch.

--- SOFII_Patcher.py
+++ SOFII_Patcher.py
@@ -1,51 +1,106 @@
 import re
+import os
 import ctypes
 import struct
+from os import path
+from glob import glob
+from winreg import ConnectRegistry, HKEY_LOCAL_MACHINE, OpenKeyEx, QueryValueEx
+
+BUFFER_SIZE = 1024
+
+GetSystemMetrics = ctypes.windll.user32.GetSystemMetrics
+GetLongPathNameW = ctypes.windll.kernel32.GetLongPathNameW
+
+
+def get_sof2_path():
+    try:
+        root = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+        policy_key = OpenKeyEx(root, r"SOFTWARE\WOW6432Node\Activision\Soldier of Fortune II - Double Helix")
+        result, _type = QueryValueEx(policy_key, "InstallPath")
+        buffer = ctypes.create_unicode_buffer(BUFFER_SIZE)
+        GetLongPathNameW(result, buffer, BUFFER_SIZE)
+        return buffer.value
+    except OSError:
+        return None
 
 
 def bytes_to_int(value):
     return struct.unpack("<I", value)[0]
 
 
-sof2mp = open("SoF2MP.exe", "rb").read()
+class SoF2(object):
+    def __init__(self, file_path="SoF2MP.exe"):
+        self.file_path = file_path
+        with open(self.file_path, "rb") as f:
+            self.file_content = f.read()
+        self.base_address = bytes_to_int(self.file_content[0x12C:0x130])
 
-# Patch Logging Function
-result = re.search(br"\xB8\x00\x10\x00\x00\xE8.{4}", sof2mp)
-if result:
-    replace_from = result.group(0)
-    replace_to = replace_from.replace(b'\xB8', b'\xC3', 1)
-    sof2mp = sof2mp.replace(replace_from, replace_to, 1)
-    print("Logging Function Patched")
-else:
-    print("Logging Function Already Patched")
+    def patch_resolution(self, width=GetSystemMetrics(0), height=GetSystemMetrics(1)):
+        # Find Resolution String
+        offset = self.file_content.find(b"Mode  3: 640x480")
+        if not offset:
+            raise Exception("Resolution String Not Found!")
 
-# Find Resolution String
-offset = sof2mp.find(b"Mode  3: 640x480")
-if not offset:
-    raise Exception("Resolution String Not Found!")
+        # Find Resolution String Reference
+        string_reference = struct.pack("<L", self.base_address + offset)
+        result = re.search(string_reference + br"(?P<width>.{4})(?P<height>.{4})\x00\x00\x80\x3F", self.file_content)
+        if not result:
+            raise Exception("Resolution Reference Not Found!")
 
-# Find Resolution String Reference
-base_address = 0x400000
-string_reference = struct.pack("<L", base_address + offset)
-result = re.search(string_reference + br"(?P<width>.{4})(?P<height>.{4})\x00\x00\x80\x3F", sof2mp)
-if not result:
-    raise Exception("Resolution Reference Not Found!")
+        print(f"Resolution Found: {bytes_to_int(result.group('width'))}, {bytes_to_int(result.group('height'))}.")
 
-print(f"Resolution Found: {bytes_to_int(result.group('width'))}, {bytes_to_int(result.group('height'))}.")
+        # Replace Resolution To System Resolution
+        replace_from = result.group(0)
+        replace_to = replace_from.replace(
+            result.group('width') + result.group('height'),
+            struct.pack('<I', width) + struct.pack('<I', height)
+        )
+        self.file_content = self.file_content.replace(replace_from, replace_to, 1)
 
-# Get System Resolution
-GetSystemMetrics = ctypes.windll.user32.GetSystemMetrics
-width, height = GetSystemMetrics(0), GetSystemMetrics(1)
+        print(f"New Resolution: {width}, {height}.")
 
-# Replace Resolution To System Resolution
-replace_from = result.group(0)
-replace_to = replace_from.replace(
-    result.group('width') + result.group('height'),
-    struct.pack('<I', width) + struct.pack('<I', height)
-)
+        return self.file_content
 
-# Save
-open("SoF2MP.exe.backup", "wb").write(sof2mp)
-open("SoF2MP.exe", "wb").write(sof2mp.replace(replace_from, replace_to, 1))
+    def patch_logging(self):
+        # Patch Logging Function
+        result = re.search(br"\xB8\x00\x10\x00\x00\xE8.{4}", self.file_content)
+        if result:
+            replace_from = result.group(0)
+            replace_to = replace_from.replace(b'\xB8', b'\xC3', 1)
+            self.file_content = self.file_content.replace(replace_from, replace_to, 1)
+            print("Logging Function Patched")
+        else:
+            print("Logging Function Already Patched")
 
-print(f"New Resolution: {width}, {height}.")
+        return self.file_content
+
+    def save(self, ext=""):
+        with open(f"{self.file_path}{ext}", "wb") as f:
+            f.write(self.file_content)
+
+    def patch(self, save_backup=True, patch_log=True, patch_res=True, res_str=""):
+        if patch_log:
+            self.patch_logging()
+
+        if patch_res:
+            kwargs = {}
+            if res_str:
+                re_res = re.search(r'(?P<width>\d+)\s*x\s*(?P<height>\d+)', res_str, re.IGNORECASE)
+                kwargs = re_res.groupdict()
+            self.patch_resolution(**kwargs)
+
+        if save_backup:
+            self.save(ext=".backup")
+        self.save()
+
+
+if __name__ == '__main__':
+    sof2_paths = list(map(path.abspath, glob('./SoF2*.exe')))
+    if not sof2_paths:
+        sof2_paths = list(glob(path.join(get_sof2_path(), 'SoF2*.exe')))
+
+    for sof2_path in sof2_paths:
+        print(path.splitext(path.basename(sof2_path))[0])
+        sof2 = SoF2()
+        sof2.patch()
+        print()
